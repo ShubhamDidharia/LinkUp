3ef74e23c3714848a714e30fc2e471fca29ea217
ShubhamDidharia <shubhamdidharia627@gmail.com>
2025-06-17 13:02:57 +0530

    followUsers + notification BE

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { protectedRoute } from '../middlewares/protectRoute.js';
+import { getUserProfile, followUnfollowUser, getSuggestedUsers, updateUserProfile } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/profile/:username',protectedRoute, getUserProfile);
 router.get('/suggested/',protectedRoute, getSuggestedUsers);
-router.post('/follow/:username',protectedRoute, followUser);
+router.post('/follow/:id',protectedRoute, followUnfollowUser);
 router.post('/update',protectedRoute, updateUserProfile);
 
 export default router;
