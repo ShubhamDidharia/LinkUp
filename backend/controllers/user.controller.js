c3bf1ccc5f2f9f19a100c2957b12d0af23101344
ShubhamDidharia <shubhamdidharia627@gmail.com>
2025-06-25 20:02:04 +0530

    added suggestedUser and user updation

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
 import Notification from '../models/notification.model.js';
+import bcrypt from 'bcryptjs';
+import {v2 as cloudinary}  from 'cloudinary';
 
 export const getUserProfile  = async(req,res)=>{
 
@@ -72,19 +74,88 @@ export const getSuggestedUsers = async(req,res)=>{
         // dont suggest yourself and already followed users
         const userId = req.user._id; // user from the protectedRoute middleware
         const followedByMe = await User.findById(userId).select('following'); 
-        
+
+        const user = await User.aggregate([
+            {
+                $match: {
+                    _id:{$ne: userId}, // Exclude the current user
+                }
+            },{$sample:{size:10}} // Randomly select 10 users
+            
+        ])
+
+        const filteredUsers = user.filter(user => ! followedByMe.following.includes(user._id)); 
+        const suggestedUsers  = filteredUsers.slice(0,4); // Limit to 4 users
+        suggestedUsers.forEach(user => user.password = undefined); // Remove password field from the suggested users
+        res.status(200).json(suggestedUsers);
     } catch (error) {
         console.error("Error fetching suggested users:", error);
         res.status(500).json({error: "Internal server error"});  
     }
     
 }
+
+
 export const updateUserProfile = async(req,res)=>{
+    const {username, fullName, email, bio, link, currentPassword, newPassword} = req.body;
+    let {profileImage, coverImage} = req.body;
+    const userId = req.user._id; // user from the protectedRoute middleware
     try {
+        let user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({error: "User not found"});
+        }
+
+        if((!currentPassword && newPasswrod) || (!newPassword && currentPassword)){
+            return res.status(400).json({error: "Both current and new passwords must be provided or neither"});
+        }
+
+        if(currentPassword && newPassword) {
+            const isMatch  = await bcrypt.compare(currentPassword, user.password);
+            if(!isMatch){
+                return res.status(400).json({error: "Current password is incorrect"});
+            }
+            if(newPassword.length < 6){
+                return res.status(400).json({error: "New password must be at least 6 characters long"});
+            }
+
+            user.password =  await bcrypt.hash(newPassword, 10); // Update the password
+        }
+
+        if(profileImage){
+            if(user.profileImage){
+                const publicId = user.profileImage.split('/').pop().split('.')[0]; // Extract public ID from the URL
+                await cloudinary.uploader.destroy(publicId); // Delete the old image from Cloudinary
+
+            }
+            // Upload the new profile image to Cloudinary
+            const uploadedResponse = await cloudinary.uploader.upload(profileImage);
+            profileImage = uploadedResponse.secure_url; // Get the secure URL of the uploaded image
+        }
+        if(coverImage){
+            if(user.coverImage){
+                const publicId = user.coverImage.split('/').pop().split('.')[0]; // Extract public ID from the URL
+                await cloudinary.uploader.destroy(publicId); // Delete the old image from Cloudinary
+            }
+            const uploadedResponse = await cloudinary.uploader.upload(coverImageImage);
+            coverImage = uploadedResponse.secure_url; // Get the secure URL of the uploaded image
+        }
+
+        user.fullName = fullName || user.fullName; // Update fullName if provided, otherwise keep the existing value
+        user.username = username || user.username; // Update username if provided, otherwise keep the existing value
+        user.email = email || user.email; // Update email if provided, otherwise keep the existing value
+        user.bio = bio || user.bio; // Update bio if provided, otherwise keep the existing value
+        user.link = link || user.link; // Update link if provided, otherwise keep the existing value
+        user.profileImage = profileImage || user.profileImage; // Update profileImage if provided, otherwise keep the existing value
+        user.coverImage = coverImage || user.coverImage; // Update coverImage if provided, otherwise keep the existing value
+        await user.save(); // Save the updated user document
+        user.password = undefined; // Remove password field from the response
+        res.status(200).json(user); // Respond with the updated user document
         
     } catch (error) {
         console.error("error updating profile : ", error);
         res.status(500).json({error: "Internal server error"});
+
     }
 }
     
--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,19 @@ import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import connectDB from './db/connectMongo.js';
 import cookieParser from 'cookie-parser';
+import {v2 as cloudinary}  from 'cloudinary';
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+
+})
 
 app.use(cookieParser()); // Middleware to parse cookies from requests
 app.use(express.json()); app.use(express.urlencoded({ extended: true }));
