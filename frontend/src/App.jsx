4aa74a51837d5de29fbe01b530cd34f72f0be545
ShubhamDidharia <shubhamdidharia627@gmail.com>
2025-06-27 12:59:16 +0530

    FE build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,26 @@
-import react from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SignUpPage from './pages/auth/signup/SignUpPage';
-import LoginPage from './pages/auth/login/LoginPage';
+import LoginPage from './pages/auth/login/LoginPage.jsx';
 import HomePage from './pages/home/HomePage';
+import Sidebar from './components/common/Sidebar';
+import RightPanel from './components/common/RightPanel.jsx';
+import NotificationPage from './pages/notification/NotificationPage..jsx';
+import ProfilePage from './pages/profile/ProfilePage';
 
 function App() {
  
   return (
     <>
       <div className='flex max-w-6xl mx-auto'>
+        <Sidebar/>
         <Routes>
           <Route path='/' element={<HomePage/>}/>
           <Route path='/login' element={<LoginPage/>}/>
           <Route path='/signup' element={<SignUpPage/>}/>
-
+          <Route path='/notifications' element={<NotificationPage/>}/>
+          <Route path='/profile/:username' element={<ProfilePage/>}/>
         </Routes>
+        <RightPanel/>
 
       </div>
     </>
